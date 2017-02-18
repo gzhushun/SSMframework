c3036072f12057a7a3dc496c2dfd1f26f99eec17
hs <gzhushun@gmail.com>
2017-02-18 15:19:27 +0800

    include google guava

---
+++ src/main/java/test/test.java
@@ -0,0 +1,12 @@
+import java.util.jar.Attributes.Name;
+
+public class test {
+	private String name;
+	public static void main(String args[]){
+		///setName("hushun");
+		System.out.println("123");
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+}
